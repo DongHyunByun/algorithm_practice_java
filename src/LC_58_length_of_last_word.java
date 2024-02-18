896958ca3524cf2f1006e80c81d048ac2e1f704d
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 <ngoodsamari@naver.com>
2024-02-18 17:53:50 +0900

    java practice

---
+++ src/LC_58_length_of_last_word.java
@@ -0,0 +1,16 @@
+public class LC_58_length_of_last_word {
+    static class Solution {
+        public int lengthOfLastWord(String s) {
+            String[] splittedString = s.split("\\s+");
+            int size = splittedString.length;
+
+            int ans = splittedString[size-1].length();
+            return ans;
+        }
+    }
+
+    public static void main(String[] args) {
+        LC_58_length_of_last_word.Solution sol = new LC_58_length_of_last_word.Solution();
+        System.out.println(sol.lengthOfLastWord("fly to the moon "));
+    }
+}
