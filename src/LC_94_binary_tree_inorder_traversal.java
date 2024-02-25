a1741dd717941395d6790ff755cb364cce60536e
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 <ngoodsamari@naver.com>
2024-02-25 12:31:27 +0900

    leet code

--- src/LC_94_binary_tree_inorder_traversal.java
+++ src/LC_94_binary_tree_inorder_traversal.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class LC_94_binary_tree_inorder_traversal {
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC_94_binary_tree_inorder_traversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static class Solution {
+        public void dfs(TreeNode node, List<Integer> list) {
+            if (node != null) {
+                dfs(node.left, list);
+                list.add(node.val);
+                dfs(node.right, list);
+            }
+        }
+
+        public List<Integer> inorderTraversal(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+            dfs(root, list);
+
+            return list;
+        }
+    }
+
 }
